d3bac8bfc95cbbdec26676619c31909be7605b10
Baianor <reis.adson2@outlook.com>
2022-06-22 16:47:49 -0300

    feat: end router POST"/products" to create product

--- src/domain/products/implementations/Create-Implementation.ts
+++ src/domain/products/implementations/Create-Implementation.ts
@@ -1,14 +1,19 @@
 import { ResultSetHeader, RowDataPacket } from 'mysql2';
 
 import connection from '../../../configs/mysql2';
+import AppError from '../../../error/appError';
 import { IProductsEntityDTO } from '../DTOs/Product-DTO';
 import {
   ICreateProductRequested,
   ICreateRepository,
 } from '../repositories/Create-Repository';
 
+interface IDataSQL extends RowDataPacket, IProductsEntityDTO {}
+
 export class CreateImplementation implements ICreateRepository {
   async create(product: ICreateProductRequested): Promise<IProductsEntityDTO> {
+    await this.verifyIfProductAlreadyExists(product.name);
+
     const Q = 'INSERT INTO StoreManager.products (name, quantity) VALUES (?,?)';
 
     const [result] = await connection.execute<ResultSetHeader>(Q, [
@@ -16,5 +21,15 @@ export class CreateImplementation implements ICreateRepository {
     ]);
 
     return { id: result.insertId, ...product };
+  }
+
+  private async verifyIfProductAlreadyExists(name: string) {
+    const Q = 'SELECT * FROM StoreManager.products WHERE name = ?';
+
+    const [result] = await connection.execute<IDataSQL[]>(Q, [name]);
+
+    if (result.length) {
+      throw new AppError(409, 'Product already exists');
+    }
   }
 }
